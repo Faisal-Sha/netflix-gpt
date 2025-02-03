8093c8dad5f360b7dfa0358a6cfbab284659ec20
faisal shahzad <faisal712000@gmail.com>
2025-02-04 01:13:10 +0500

    sign up/sign in form basic setup done

---
+++ src/components/Body.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Login';
+import Browse from './Browse';
+
+const Body = () => {
+  return (
+    // <RouterProvider router={appRouter} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/browse" element={<Browse />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default Body;
