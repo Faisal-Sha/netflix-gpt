d243da64d714b97e4a2010ff3996a8f5ed61871e
faisal shahzad <faisal712000@gmail.com>
2025-02-07 11:54:03 +0500

    removed bugs and updated readme and added example env file

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,4 +1,3 @@
-import { useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
 import openai from "../utils/openai";
 import { useRef } from "react";
@@ -38,7 +37,7 @@ const GptSearchBar = () => {
     return (
         <div className="pt-[35%] md:pt-[10%] flex justify-center">
             <form className="w-full md:w-1/2 bg-black grid grid-cols-12" onSubmit={(e) => e.preventDefault()}>
-                <input ref={searchText} type="text" className=" p-4 m-4 col-span-9" placeholder={lang[langKey].gptSearchPlaceholder} />
+                <input ref={searchText} type="text" className=" p-4 m-4 col-span-9 bg-white" placeholder={lang[langKey].gptSearchPlaceholder} />
                 <button onClick={handleGptSearchClick} className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">{lang[langKey].search}</button>
             </form>
         </div>
