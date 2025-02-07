aa87361eabd0b5d36d134ca08aeeba1fa3c90e63
faisal shahzad <faisal712000@gmail.com>
2025-02-07 11:20:56 +0500

    Made the app responsive for mobile

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -18,7 +18,6 @@ const GptSearchBar = () => {
         return json.results;
     };
     const handleGptSearchClick = async () => {
-        console.log(searchText.current.value);
         // Make an API call to GPT API and get Movie Results
         const gptQuery = "Act as a Movie Recommendation system and suggest some movies for the query : " + searchText.current.value + ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
         const gptResults = await openai.chat.completions.create({
@@ -37,8 +36,8 @@ const GptSearchBar = () => {
     };
 
     return (
-        <div className="pt-[10%] flex justify-center">
-            <form className=" w-1/2 bg-black grid grid-cols-12" onSubmit={(e) => e.preventDefault()}>
+        <div className="pt-[35%] md:pt-[10%] flex justify-center">
+            <form className="w-full md:w-1/2 bg-black grid grid-cols-12" onSubmit={(e) => e.preventDefault()}>
                 <input ref={searchText} type="text" className=" p-4 m-4 col-span-9" placeholder={lang[langKey].gptSearchPlaceholder} />
                 <button onClick={handleGptSearchClick} className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">{lang[langKey].search}</button>
             </form>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -75,15 +75,9 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
-        <img
-          src={BACKGROUND_IMG}
-          alt="background-image"
-        />
+        <img className="h-screen object-cover" src={BACKGROUND_IMG} alt="background-image" />
       </div>
-      <form
-        onSubmit={(e) => e.preventDefault()}
-        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
-      >
+      <form onSubmit={(e) => e.preventDefault()} className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
