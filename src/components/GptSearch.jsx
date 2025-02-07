fb39190b09f976a6456eb00c0c6b0b85d9edaf0e
faisal shahzad <faisal712000@gmail.com>
2025-02-07 11:54:03 +0500

    removed bugs and updated readme and added example env file

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,11 +1,11 @@
-import { BG_URL } from "../utils/constants";
+import { BACKGROUND_IMG } from "../utils/constants";
 import GptMovieSuggestions from "./GptMovieSuggestions";
 import GptSearchBar from "./GptSearchBar";
 const GPTSearch = () => {
   return (
     <>
       <div className="fixed -z-10">
-        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
+        <img className="h-screen object-cover" src={BACKGROUND_IMG} alt="logo" />
       </div>
       <div className="">
         <GptSearchBar />
